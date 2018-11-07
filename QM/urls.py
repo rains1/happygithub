a9780477b8a322f9a486f8c304675b4ef087d270
hll <hll@qq.com>
2018-11-07 21:21:28 +0800

    clean

--- QM/urls.py
+++
@@ -1,8 +0,0 @@
-
-import xadmin as admin
-from django.conf.urls import url,include
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^ueditor/', include('DjangoUeditor.urls')),
-]
